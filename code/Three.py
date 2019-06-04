76656a173c6a2f92863ac3ba0cf240ef2dd5209b
ArunaRanjitha <51354222+ArunaRanjitha@users.noreply.github.com>
2019-06-04 13:16:34 +0530

    Create Three.py

---
+++ code/Three.py
@@ -0,0 +1,10 @@
+ch=str(input())
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    if(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+        print("Vowel")
+    elif(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+   print("invalid")  
