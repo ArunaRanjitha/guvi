4bb69dad3a964ac04a26694a8b7cf98463195699
ArunaRanjitha <51354222+ArunaRanjitha@users.noreply.github.com>
2019-08-10 21:19:42 +0530

    Update set2_3.py

--- set2_3.py
+++ set2_3.py
@@ -2,7 +2,7 @@ n = int(input())
 if n > 1:
     for i in range(2, n):
         if (n % i) == 0:
-            print("yes")
+            print("no")
             break
     else:
-        print("no")
+        print("yes")
